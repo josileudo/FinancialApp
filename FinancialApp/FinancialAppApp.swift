ebbb2565274cfed8e5e428dc2387d32a02b49bc3
Josileudo <josileudorodrigues75@gmail.com>
2022-07-28 10:10:38 -0300

    Initial Commit

---
+++ FinancialApp/FinancialAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FinancialAppApp.swift
+//  FinancialApp
+//
+//  Created by Josileudo Rodrigues on 28/07/22.
+//
+
+import SwiftUI
+
+@main
+struct FinancialAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
