f73aef1efbc27f16dd83bc96fa6260f3c082b108
Josileudo <josileudorodrigues75@gmail.com>
2022-08-03 12:06:06 -0300

    :lipstick: Enumerated itens >> Create a form to organize items added.

--- FinancialApp/RecentTransactionList.swift
+++ FinancialApp/RecentTransactionList.swift
@@ -31,17 +31,38 @@ struct RecentTransactionList: View {
                 }
             }
             .padding(.top);
+            
+            // MARK: Recent Transactions List
+            ForEach(Array(transactionListVM.transactions.prefix(5).enumerated()), id: \.element) { index, transaction in
+                TransactionRow(transaction: transaction)
+                
+                Divider()
+                    .opacity(index == 4 ? 0 : 1);
+            }
         }
         .padding()
         .background(Color.systemBackground)
         .clipShape(RoundedRectangle(cornerRadius: 20, style: .continuous))
         .shadow(color: Color.primary.opacity(0.2), radius: 10, x: 0, y: 5)
       }
+    
         
     }
 
 struct RecentTransactionList_Previews: PreviewProvider {
+    static let transactionListVM: TransactionListViewModel = {
+        let transactionListVM = TransactionListViewModel();
+        transactionListVM.transactions = transactionListPreviewData;
+        return transactionListVM;
+    }()
+    
     static var previews: some View {
-        RecentTransactionList();
+        Group {
+            RecentTransactionList()
+            RecentTransactionList()
+                .preferredColorScheme(.dark);
+        }
+        .environmentObject(transactionListVM);
+        
     }
 }
--- FinancialApp/TransactionModel.swift
+++ FinancialApp/TransactionModel.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-struct Transaction: Identifiable, Decodable {
+struct Transaction: Identifiable, Decodable, Hashable {
     let id: Int
     let date: String
     let institution: String
