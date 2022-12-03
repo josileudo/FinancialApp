80fb8991c69a19a8a8be83b8c45caa56729bb3ce
Josileudo <josileudorodrigues75@gmail.com>
2022-12-03 18:00:50 -0300

    :lipstick: feat: Starting form values

--- FinancialApp/views/TransactionModal.swift
+++ FinancialApp/views/TransactionModal.swift
@@ -10,8 +10,10 @@ import SwiftUI
 struct TransactionModal: View {
     @Binding var isPresented: Bool;
     @State private var value: String = "R$ 0,00";
-    @State private var menu: String = ""
-    
+    @State private var menu: String = "";
+    @State private var date = Date();
+    @State private var toggleSwitch: Bool = false;
+    @State private var describerField: String = "";
     
     var body: some View {
         let columns = Array(
@@ -39,7 +41,7 @@ struct TransactionModal: View {
                 .foregroundColor(Color.white)
                 .padding([.leading, .trailing])
                 
-                
+                // MARK: Receiver value
                 VStack(alignment: .leading ) {
                     Text("Digite sua Receita")
                         .font(.system(size: 14, weight: .semibold))
@@ -51,8 +53,71 @@ struct TransactionModal: View {
                 .foregroundColor(Color.white)
                 .padding()
                 
+                //MARK: Screen contains form informations
                 VStack() {
-                    Text("Screen 2")
+                    
+                    VStack(alignment: .leading, spacing: 12){
+                        
+                        
+                        // MARK: Toggle button
+                        Toggle(isOn: $toggleSwitch) {
+                            Image(systemName: "checkmark.circle")
+                                .font(.system(size: 22))
+                                .padding(.trailing, 6)
+                            
+                            Text("Recebido")
+                                .font(.system(size: 18))
+                        }
+                        .toggleStyle(SwitchToggleStyle(tint: Color.expense))
+                        .frame(minHeight: 40)
+                        
+                        Divider()
+                        
+                        //MARK: DatePicker component
+                        DatePicker( selection: $date, in: Date()..., displayedComponents: [.date]) {
+                            Image(systemName: "calendar.badge.plus")
+                                .font(.system(size: 22))
+                                .padding(.trailing, 6)
+                            
+                            Text("Data")
+                                .font(.system(size: 18))
+                        }
+                        .frame(minHeight: 40)
+                        Divider()
+                        
+                        //MARK: Describer component
+                        HStack() {
+                            Image(systemName: "pencil")
+                                .font(.system(size: 22))
+                                .padding(.trailing, 6)
+                            
+                            TextField("Descrição", text: $describerField)
+                        }
+                        .frame(minHeight: 40)
+                        
+                        Divider()
+                        
+                        //MARK: Categories
+                        
+                        
+                    }
+                    .padding(.top, 15)
+                   
+                    Spacer()
+                
+                    //MARK: SAVE BUTTON
+                    Button(action: {
+                        
+                    }, label: {
+                        Text("Salvar")
+                    })
+                    .padding()
+                    .background(Color.expense)
+                    .foregroundColor(Color.white)
+                    .font(.system(size: 18, weight: .bold))
+                    .cornerRadius(10)
+                   // .scaleEffect(configuration.isPressed ? 1.2 : 1)
+                                        
                 }
                 .padding(15)
                 .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .top)
