738e96960efa1c9e18e91eba18563f9f07951743
Josileudo <josileudorodrigues75@gmail.com>
2023-01-15 15:47:05 -0300

    :sparkles: feat: Add a method to load data

--- FinancialApp/stores/RegisterStore.swift
+++ FinancialApp/stores/RegisterStore.swift
@@ -18,4 +18,29 @@ class RegisterStore: ObservableObject {
                                     create: false)
         .appendingPathComponent("register.data")
     }
+    
+    static func load(completion: @escaping(Result<[Register], Error>) -> Void){
+        DispatchQueue.global(qos: .background).async {
+            do {
+                let fileUrl = try fileUrl();
+                guard let file = try? FileHandle(forReadingFrom: fileUrl) else {
+                    DispatchQueue.main.async {
+                        completion(.success([]));
+                        print(completion(.success([])));
+                    }
+                    return;
+                }
+                let register = try JSONDecoder().decode([Register].self, from: file.availableData);
+                DispatchQueue.main.async {
+                    completion(.success(register));
+                    print(completion(.success(register)));
+                }
+            } catch {
+                DispatchQueue.main.async {
+                    completion(.failure(error));
+                    print(completion(.failure(error)));
+                }
+            }
+        }
+    }
 }
